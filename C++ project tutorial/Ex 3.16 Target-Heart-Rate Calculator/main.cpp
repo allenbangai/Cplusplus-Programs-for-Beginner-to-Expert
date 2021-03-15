5ab067beff262e51345e36816020c16547a2e003
=allenbangai <=allenkamadje@gmail.com>
2021-03-15 01:10:15 +0100

    creating C++ application for Target-Heart-Rate Calculator

---
+++ C++ project tutorial/Ex 3.16 Target-Heart-Rate Calculator/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: DELL
+ *
+ * Created on March 15, 2021, 1:00 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
