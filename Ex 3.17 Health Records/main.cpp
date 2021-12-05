e44f958526d835f7285bcf63d59caccdf2a6b63f
allenbangai <allenkamadje@gmail.com>
2021-12-05 23:28:54 +0000

    creating Health Record project for exercise 3.17

---
+++ Ex 3.17 Health Records/main.cpp
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/main.cc to edit this template
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: DELL
+ *
+ * Created on December 6, 2021, 12:14 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
