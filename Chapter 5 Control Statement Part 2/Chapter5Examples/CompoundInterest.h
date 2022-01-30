f1c0d9c651e9ed881eb7bbb1e942459463a83d72
allenbangai <allenkamadje@gmail.com>
2022-01-30 00:00:46 +0000

    creating header file CompoundInterest.h

---
+++ Chapter 5 Control Statement Part 2/Chapter5Examples/CompoundInterest.h
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/file.h to edit this template
+ */
+
+/* 
+ * File:   CompoundInterest.h
+ * Author: DELL
+ *
+ * Created on January 30, 2022, 12:51 AM
+ */
+
+#ifndef COMPOUNDINTEREST_H
+#define COMPOUNDINTEREST_H
+
+
+
+#endif /* COMPOUNDINTEREST_H */
+
---
+++ Chapter 5 Control Statement Part 2/Chapter5Examples/main.cpp
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/main.cc to edit this template
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: DELL
+ *
+ * Created on January 30, 2022, 12:40 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
