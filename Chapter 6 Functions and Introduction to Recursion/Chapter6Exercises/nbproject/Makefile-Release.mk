6d91dd145ebee49db55949cc48d84cf5f20ee443
allenbangai <allenkamadje@gmail.com>
2022-02-08 22:56:50 +0000

    creating class ScaleRandomInteger

--- Chapter 6 Functions and Introduction to Recursion/Chapter6Exercises/nbproject/Makefile-Release.mk
+++ Chapter 6 Functions and Introduction to Recursion/Chapter6Exercises/nbproject/Makefile-Release.mk
@@ -35,6 +35,7 @@ OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
 
 # Object Files
 OBJECTFILES= \
+	${OBJECTDIR}/ScaleRandomIntegers.o \
 	${OBJECTDIR}/main.o
 
 
@@ -61,6 +62,11 @@ LDLIBSOPTIONS=
 ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/chapter6exercises.exe: ${OBJECTFILES}
 	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
 	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/chapter6exercises ${OBJECTFILES} ${LDLIBSOPTIONS}
+
+${OBJECTDIR}/ScaleRandomIntegers.o: ScaleRandomIntegers.cpp
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.cc) -O2 -std=c++14 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/ScaleRandomIntegers.o ScaleRandomIntegers.cpp
 
 ${OBJECTDIR}/main.o: main.cpp
 	${MKDIR} -p ${OBJECTDIR}
