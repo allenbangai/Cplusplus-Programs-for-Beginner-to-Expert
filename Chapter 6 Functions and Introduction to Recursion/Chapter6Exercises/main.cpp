e01521f1daf42aafd1c35d8ef27cff2ad5d207c3
allenbangai <allenkamadje@gmail.com>
2022-02-08 22:54:15 +0000

    creating chapter 6 Exercises folder for chapter 6 examples program and some exercises

---
+++ Chapter 6 Functions and Introduction to Recursion/Chapter6Exercises/main.cpp
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/main.cc to edit this template
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Kamadje Allen
+ *
+ * Created on February 8, 2022, 1:26 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
