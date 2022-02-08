a621e6dd623cfcda7353a57d05c2f2d6a62a3580
allenbangai <allenkamadje@gmail.com>
2022-02-08 23:12:43 +0000

    implementing method scaleRandomNumber in RandomInteger.cpp  part 2

--- Chapter 6 Functions and Introduction to Recursion/Chapter6Exercises/RandomIntegers.cpp
+++ Chapter 6 Functions and Introduction to Recursion/Chapter6Exercises/RandomIntegers.cpp
@@ -19,8 +19,17 @@ RandomIntegers::RandomIntegers(const RandomIntegers& orig) {
 }
 
 void RandomIntegers::scaleRandomIntegers(){
-    
-}
+    //loop 20 times
+    for(int i=0; i<20; i++){
+        //pick random number from one to six
+        cout<<setw(10)<<(1+rand()%6);
+       
+        //if counter is divisible by 5, start a new line
+        if(i%5 == 0){
+            cout<<endl;
+        }
+    }//end for
+}//end method
 
 RandomIntegers::~RandomIntegers() {
 }
