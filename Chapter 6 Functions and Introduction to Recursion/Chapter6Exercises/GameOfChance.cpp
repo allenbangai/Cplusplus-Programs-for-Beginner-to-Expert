5741969f546cd808590f6fdd509cc4bb66e02ddd
allenbangai <allenkamadje@gmail.com>
2022-02-12 20:04:51 +0000

    creating class GameOfChance with header and source file

---
+++ Chapter 6 Functions and Introduction to Recursion/Chapter6Exercises/GameOfChance.cpp
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/class.cc to edit this template
+ */
+
+/* 
+ * File:   GameOfChance.cpp
+ * Author: DELL
+ * 
+ * Created on February 12, 2022, 9:03 PM
+ */
+
+#include "GameOfChance.h"
+
+GameOfChance::GameOfChance() {
+}
+
+GameOfChance::GameOfChance(const GameOfChance& orig) {
+}
+
+GameOfChance::~GameOfChance() {
+}
+
---
+++ Chapter 6 Functions and Introduction to Recursion/Chapter6Exercises/GameOfChance.h
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/class.h to edit this template
+ */
+
+/* 
+ * File:   GameOfChance.h
+ * Author: DELL
+ *
+ * Created on February 12, 2022, 9:03 PM
+ */
+
+#ifndef GAMEOFCHANCE_H
+#define GAMEOFCHANCE_H
+
+class GameOfChance {
+public:
+    GameOfChance();
+    GameOfChance(const GameOfChance& orig);
+    virtual ~GameOfChance();
+private:
+
+};
+
+#endif /* GAMEOFCHANCE_H */
+
