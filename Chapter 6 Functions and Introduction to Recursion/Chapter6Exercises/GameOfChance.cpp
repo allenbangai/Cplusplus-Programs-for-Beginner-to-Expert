90bb08141045e2d95c1a799b09db693170f1bbe5
allenbangai <allenkamadje@gmail.com>
2022-02-13 23:56:44 +0000

    ducumenting method playStart part 2

--- Chapter 6 Functions and Introduction to Recursion/Chapter6Exercises/GameOfChance.cpp
+++ Chapter 6 Functions and Introduction to Recursion/Chapter6Exercises/GameOfChance.cpp
@@ -30,6 +30,14 @@ int GameOfChance::rollDiece(){
 
 /**
  * Code implementation for game of chance... The game start as follows
+ * 
+ * A player rolls two dice. Each die has six faces. These faces contain 1, 2, 3, 4, 5 and 6
+ * spots. After the dice have come to rest, the sum of the spots on the two upward faces is
+ * calculated. If the sum is 7 or 11 on the first roll, the player wins. If the sum is 2, 3 or
+ * 12 on the first roll (called “craps”), the player loses (i.e., the “house” wins). If the sum
+ * is 4, 5, 6, 8, 9 or 10 on the first roll, then that sum becomes the player’s “point.” To
+win, you must continue rolling the dice until you “make your point.” The player loses
+by rolling a 7 before making the point.
  */
 void GameOfChance::playStart(){
     
