044c054f3080da115480348f0d3d12f0e52774f3
allenbangai <allenkamadje@gmail.com>
2022-02-23 17:46:33 +0000

    creating class CAI (computer assisted instructions) with author name

--- Chapter 6 Functions and Introduction to Recursion/Ex 6.65 to 6.60 Computer Assisted Instructions/nbproject/Makefile-Release.mk
+++ Chapter 6 Functions and Introduction to Recursion/Ex 6.65 to 6.60 Computer Assisted Instructions/nbproject/Makefile-Release.mk
@@ -35,6 +35,7 @@ OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
 
 # Object Files
 OBJECTFILES= \
+	${OBJECTDIR}/CAI.o \
 	${OBJECTDIR}/main.o
 
 
@@ -61,6 +62,11 @@ LDLIBSOPTIONS=
 ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/ex_6.65_to_6.60_computer_assisted_instructions.exe: ${OBJECTFILES}
 	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
 	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/ex_6.65_to_6.60_computer_assisted_instructions ${OBJECTFILES} ${LDLIBSOPTIONS}
+
+${OBJECTDIR}/CAI.o: CAI.cpp
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.cc) -O2 -std=c++14 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/CAI.o CAI.cpp
 
 ${OBJECTDIR}/main.o: main.cpp
 	${MKDIR} -p ${OBJECTDIR}
