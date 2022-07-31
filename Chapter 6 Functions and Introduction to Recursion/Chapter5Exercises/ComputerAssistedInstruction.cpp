ce7019fab103d0a5c6d9544c9ca201ea4a9f69e6
allenbangai <allenkamadje@gmail.com>
2022-07-31 12:10:09 +0100

    imlpementing Exercise 6.56 part 1

--- Chapter 6 Functions and Introduction to Recursion/Chapter5Exercises/ComputerAssistedInstruction.cpp
+++ Chapter 6 Functions and Introduction to Recursion/Chapter5Exercises/ComputerAssistedInstruction.cpp
@@ -3,3 +3,24 @@
 //
 
 #include "ComputerAssistedInstruction.h"
+
+int ComputerAssistedInstruction::getVal1() const {
+    return val1;
+}
+
+int ComputerAssistedInstruction::getVal2() const {
+    return val2;
+}
+
+void ComputerAssistedInstruction::setVal1(int val1) {
+    ComputerAssistedInstruction::val1 = val1;
+}
+
+void ComputerAssistedInstruction::setVal2(int val2) {
+    ComputerAssistedInstruction::val2 = val2;
+}
+
+void ComputerAssistedInstruction::start() {
+
+
+}
--- Chapter 6 Functions and Introduction to Recursion/Chapter5Exercises/ComputerAssistedInstruction.h
+++ Chapter 6 Functions and Introduction to Recursion/Chapter5Exercises/ComputerAssistedInstruction.h
@@ -2,12 +2,32 @@
 // Created by Allen on 30/07/2022.
 //
 
+#include <iomanip>
+#include <iostream>
+
 #ifndef CHAPTER_6_FUNCTIONS_AND_INTRODUCTION_TO_RECURSION_COMPUTERASSISTEDINSTRUCTION_H
 #define CHAPTER_6_FUNCTIONS_AND_INTRODUCTION_TO_RECURSION_COMPUTERASSISTEDINSTRUCTION_H
 
 
 class ComputerAssistedInstruction {
+private:
+    int val1;
+    int val2;
 
+public:
+    ComputerAssistedInstruction();
+
+    bool productCheck();
+
+    int getVal1() const;
+
+    int getVal2() const;
+
+    void setVal1(int val1);
+
+    void setVal2(int val2);
+
+    void start();
 };
 
 
