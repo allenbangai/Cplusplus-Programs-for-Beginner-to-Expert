5e672959a6b8165f5f637c2965385e924469ab13
allenbangai <allenkamadje@gmail.com>
2022-07-31 14:00:30 +0100

    imlpementing Exercise 6.56 part 2

--- Chapter 6 Functions and Introduction to Recursion/Chapter5Exercises/ComputerAssistedInstruction.cpp
+++ Chapter 6 Functions and Introduction to Recursion/Chapter5Exercises/ComputerAssistedInstruction.cpp
@@ -4,23 +4,42 @@
 
 #include "ComputerAssistedInstruction.h"
 
-int ComputerAssistedInstruction::getVal1() const {
+auto ComputerAssistedInstruction::getVal1() const {
     return val1;
 }
 
-int ComputerAssistedInstruction::getVal2() const {
+auto ComputerAssistedInstruction::getVal2() const {
     return val2;
 }
 
-void ComputerAssistedInstruction::setVal1(int val1) {
-    ComputerAssistedInstruction::val1 = val1;
-}
-
-void ComputerAssistedInstruction::setVal2(int val2) {
-    ComputerAssistedInstruction::val2 = val2;
-}
-
 void ComputerAssistedInstruction::start() {
+    while (constVal != 0){
+        srand(time(0));
+        val1 = rand()%9 + 1;
+        val2 = rand()%9 + 1;
+        auto answer = 9;
+        do{
+            cout<<"How much is "<<val1<<" times "<<val2<<"? ";
+            cin>>answer;
+            cout<<endl;
+            if(productCheck(answer)){
+                cout<<"Very Good, take another exercise"<<endl;
+            }else{
+                cout<<"No. Please try again"<<endl;
+            }
 
+        } while (!productCheck(answer));
+        cout<<"enter 0 to stop and 1 to continue: ";
+        cin>>constVal;
+        cout<<endl;
+    }
 
+}
+
+bool ComputerAssistedInstruction::productCheck(int val) {
+    if(val == getVal1() * getVal2()){
+        return true;
+    }else{
+        return false;
+    }
 }
--- Chapter 6 Functions and Introduction to Recursion/Chapter5Exercises/ComputerAssistedInstruction.h
+++ Chapter 6 Functions and Introduction to Recursion/Chapter5Exercises/ComputerAssistedInstruction.h
@@ -4,24 +4,28 @@
 
 #include <iomanip>
 #include <iostream>
+#include <random>
+#include <cstdlib>
 
 #ifndef CHAPTER_6_FUNCTIONS_AND_INTRODUCTION_TO_RECURSION_COMPUTERASSISTEDINSTRUCTION_H
 #define CHAPTER_6_FUNCTIONS_AND_INTRODUCTION_TO_RECURSION_COMPUTERASSISTEDINSTRUCTION_H
 
+using namespace std;
 
 class ComputerAssistedInstruction {
 private:
+    int constVal = 1;
     int val1;
     int val2;
 
 public:
     ComputerAssistedInstruction();
 
-    bool productCheck();
+    bool productCheck(int );
 
-    int getVal1() const;
+    auto getVal1() const;
 
-    int getVal2() const;
+    auto getVal2() const;
 
     void setVal1(int val1);
 
